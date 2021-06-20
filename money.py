904e8260115522024369ff2dbafca9b14d004d52
qqxufeng <172422451@qq.com>
2021-06-21 00:30:18 +0800

    主程序计算价格
    学习Python,简单的条件分支语句

---
+++ money.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+money = float (input("请输入机票价格："))
+pay_1 = (money - 10000) * .3
+pay_2 = (money - 20000) * .7 + 3000
+pay_3 = (money - 25000)  + 6500
+if money <= 10000 :
+    print('你回国机票公司承担！')
+elif money <= 20000 :
+    print('你回国，自行应付机票:','%.2f' %(pay_1))
+elif money <= 25000 :
+    print('你回国，自行应付机票:','%.2f' %(pay_2))
+else:
+    print('你回国，自行应付机票:','%.2f' %(pay_3))
+    
+    
+    
+    
+    
