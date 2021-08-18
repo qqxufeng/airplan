fea06e626e4f90a95ce4f80271fec5ec62b05fb9
qqxufeng <172422451@qq.com>
2021-08-19 05:46:39 +0800

    pyinstaller打包exe 运行之后不会闪退
    pip install pyinstaller
    
    pyinstaller -F *.py

--- money.py
+++ money.py
@@ -6,7 +6,7 @@
   条件循环语句
 2. '%.2f' % 保留2位小数
 """
-
+import os
 money = float (input("请输入机票价格："))
 pay_1 = (money - 10000) * .3
 pay_2 = (money - 20000) * .7 + 3000
@@ -19,7 +19,7 @@ elif money <= 25000 :
     print('你回国，自行应付机票:','%.2f' %(pay_2))
 else:
     print('你回国，自行应付机票:','%.2f' %(pay_3))
-    
+os.system("pause")   
     
     
     
